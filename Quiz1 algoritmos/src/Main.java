5844578f1576c859e49a2020fbe644bfee05ebd1
Luisa Marinez <luisa.marinez1@correo.icesi.edu.co>
2021-04-11 22:55:04 -0500

    actualizacion de repo

---
+++ Quiz1 algoritmos/src/Main.java
@@ -0,0 +1,34 @@
+
+
+
+import processing.core.PApplet;
+
+public class Main extends PApplet {
+	
+
+	public static void main(String[]args) {
+		
+	}
+	
+	public void settings() {
+		size(500,500);
+		
+	}
+	
+	public void setup() {
+		
+		
+	}
+	
+	public void draw() {
+		background (255);
+		
+	}
+
+	public void mousePressed() {
+		
+	}
+	
+	
+
+}//FINAL 
